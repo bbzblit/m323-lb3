925580615bcac4c8e8ad6a83d6166dc606ef5dc4
Yannick Müller <mail@ynnk.dev>
2023-10-04 20:10:37 +0200

    bgufix

--- imperativ/stats/statistics.py
+++ imperativ/stats/statistics.py
@@ -8,7 +8,7 @@ def get_statistics_of_train(date: date, train: str):
     df = df[df["LINIEN_TEXT"] == train]
     df = df[df["DELAY_ABFAHRT"].isna()]
     
-    df[df["DELAY_ANKUFT"] < 0]  = 0
+    df["DELAY_ANKUFT"][df["DELAY_ANKUFT"] < 0] = 0
 
     stops = df["HALTESTELLEN_NAME"].unique().tolist()
 
@@ -32,9 +32,10 @@ def get_delay_of_exact_connection(date: date, train_line: str, time: str):
     fahrt_id = df[df["ABFAHRTSZEIT"] == time].iloc[0]["FAHRT_ID"]
     df = df[df["FAHRT_ID"] == fahrt_id]
     stops = df["HALTESTELLEN_NAME"].unique().tolist()
+    
+    df["DELAY_ANKUFT"][df["DELAY_ANKUFT"] < 0] = 0
+    df["DELAY_ABFAHRT"][df["DELAY_ABFAHRT"] < 0] = 0
 
-    df[df["DELAY_ANKUFT"] < 0]  = 0
-    df[df["DELAY_ABFAHRT"] < 0]  = 0
     return {
         "meta":  {
             "Train": train_line,
